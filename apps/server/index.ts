dd2f208f12d074a891435957d2538899714c0945
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-26 17:30:55 +0600

    authRouter

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,11 +1,22 @@
 import express, { Request, Response } from "express";
 
+import cors from "cors";
+import authRouter from "./routes/userRout";
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
+// apis
+
+app.use("/api/register", authRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
