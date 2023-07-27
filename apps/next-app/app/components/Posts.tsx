1d3443d72dac1e9c7d77fc6b62496cc09613e03c
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-27 10:36:57 +0600

    posts fetched

--- apps/next-app/app/components/Posts.tsx
+++ apps/next-app/app/components/Posts.tsx
@@ -1,4 +1,15 @@
 "use client";
+
+import { useEffect } from "react";
+
 export default function Posts() {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("http://localhost:5000/api/post");
+      const data = await res.json();
+      console.log(data);
+    };
+    fetchPosts();
+  }, []);
   return <div>Posts</div>;
 }
